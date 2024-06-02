c6f4d7084151bea0eb777a6f4540a2a8521fdad0
Dharu Aulia Rahman <106579507+hanyarui@users.noreply.github.com>
2024-06-02 22:32:11 +0700

    week 2

---
+++ app/src/main/java/com/dicoding/capstone/ui/forgotPassword/OtpActivity.kt
@@ -0,0 +1,20 @@
+package com.dicoding.capstone.ui.forgotPassword
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.capstone.R
+import com.dicoding.capstone.databinding.ActivityOtpBinding
+
+class OtpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOtpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityOtpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
